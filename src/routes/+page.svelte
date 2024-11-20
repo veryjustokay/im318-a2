620ae21b9523e510294a5c41766d97d51d1a0c2b
veryjustokay <sconstan@confederationcollege.ca>
2024-11-20 16:05:13 -0500

    added functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+     import Todo from "./Todo.svelte"
+</script>
+     
+<h1>
+     To Do:
+</h1> 
+<Todo />
---
+++ src/routes/Todo.svelte
@@ -0,0 +1,29 @@
+<script>
+     let todoItem = $state('');
+     let todoList = $state([]);
+
+function addItem() {
+     event.preventDefault();
+     if (todoItem == '') {
+          return;
+     }
+     todoList.push(todoItem);
+     todoItem = '';
+}
+
+$inspect(todoList);
+</script>
+<form onsubmit={addItem}>
+<input type="text" bind:value={todoItem}>
+<button type="submit">Add</button>
+</form>
+
+<ul>
+     {#each todoList as item}
+          <li>{item}</li>
+     {/each}
+</ul>
+
+<style>
+
+</style>
